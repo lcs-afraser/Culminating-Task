490ce1be74c1b5a343ff205615cc2f7bea83e26c
Alistair Fraser <afraser@lcs.on.ca>
2023-06-02 09:51:49 -0400

    Search function works and shows album name of top of artist and song name

--- Culminating Task/VIews/PlaylistView.swift
+++ Culminating Task/VIews/PlaylistView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct PlaylistView: View {
+    
+    //MARK: Stored Properties
+    
+    //Holds the list of songs returned by our search
+    @State var foundSongs: [Song] = []
+    
+    //MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // Show a list of songs that match the current search term
+        List(foundSongs, id: \.trackId) { currentSong in
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(currentSong.trackName)
+                        .bold()
+                    
+                    Spacer()
+                }
+                Text(currentSong.collectionName)
+                    .italic()
+                
+                Text(currentSong.artistName)
+            }
+        }
+        .task {
+            //When the view appears fetch search results for AntiHero
+            foundSongs = await NetworkService.fetch()
+        }
+//        .toolbar {
+//            ToolbarItem(placement: .primaryAction) {
+//                Button(action: {
+//                    showingAddMovieView = true
+//                }, label: {
+//                    Image(systemName: "plus")
+//                })
+//                .sheet(isPresented: $showingAddMovieView) {
+//                    AddMovieView()
+//                        .presentationDetents([.fraction(0.3)])
+//                }
+//            }
+//        }
+        
     }
 }
 
