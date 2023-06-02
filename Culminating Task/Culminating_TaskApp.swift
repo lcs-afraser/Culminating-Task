e167a5d04051ba5fd94b05db7ba46c0ec65174a5
Alistair Fraser <afraser@lcs.on.ca>
2023-06-02 08:54:41 -0400

    Initial Commit

---
+++ Culminating Task/Culminating_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_TaskApp.swift
+//  Culminating Task
+//
+//  Created by Alistair Fraser on 2023-06-02.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
