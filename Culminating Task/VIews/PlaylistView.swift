55b9cd14e0d00e2d234d2b71420f6c66ff13ca75
Alistair Fraser <afraser@lcs.on.ca>
2023-06-02 09:27:02 -0400

    Removed content view and made PlaylistView

---
+++ Culminating Task/VIews/PlaylistView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaylistView.swift
+//  Culminating Task
+//
+//  Created by Alistair Fraser on 2023-06-02.
+//
+
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaylistView()
+    }
+}
