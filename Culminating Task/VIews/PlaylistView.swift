6ad93bfd38b552a4c5137568489b4c80d7e980fd
Alistair Fraser <afraser@lcs.on.ca>
2023-06-02 09:59:08 -0400

    Search bar appears but it is not functional

--- Culminating Task/VIews/PlaylistView.swift
+++ Culminating Task/VIews/PlaylistView.swift
@@ -14,29 +14,37 @@ struct PlaylistView: View {
     //Holds the list of songs returned by our search
     @State var foundSongs: [Song] = []
     
+    //Holds the song name that is being searched
+    @State var searchText = ""
+    
     //MARK: Computed Properties
     var body: some View {
         
-        // Show a list of songs that match the current search term
-        List(foundSongs, id: \.trackId) { currentSong in
-            
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(currentSong.trackName)
-                        .bold()
-                    
-                    Spacer()
-                }
-                Text(currentSong.collectionName)
-                    .italic()
+        
+        NavigationView {
+            List(foundSongs, id: \.trackId) { currentSong in
                 
-                Text(currentSong.artistName)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(currentSong.trackName)
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    Text(currentSong.collectionName)
+                        .italic()
+                    
+                    Text(currentSong.artistName)
+                }
+            }
+            .searchable(text: $searchText)
+            .task {
+                //When the view appears fetch search results
+                foundSongs = await NetworkService.fetch(resultsFor: "As it was")
             }
         }
-        .task {
-            //When the view appears fetch search results
-            foundSongs = await NetworkService.fetch(resultsFor: "As it was")
-        }
+        // Show a list of songs that match the current search term
+
 //        .toolbar {
 //            ToolbarItem(placement: .primaryAction) {
 //                Button(action: {
