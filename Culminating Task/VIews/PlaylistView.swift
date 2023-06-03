675ea8a66b7793ada1073dfb4c513ecd9692f787
Alistair Fraser <afraser@lcs.on.ca>
2023-06-03 11:41:25 -0400

    Searching works with audio

--- Culminating Task/VIews/PlaylistView.swift
+++ Culminating Task/VIews/PlaylistView.swift
@@ -15,7 +15,7 @@ struct PlaylistView: View {
     @State var foundSongs: [Song] = []
     
     //Holds the song name that is being searched
-    @State var searchText = "Money City Maniacs"
+    @State var searchText = ""
     
     //MARK: Computed Properties
     var body: some View {
@@ -67,18 +67,49 @@ struct PlaylistView: View {
 //                }
 //            }
             VStack {
-                Text(searchText)
-                    .onAppear {
-                        Task {
-                            //Fetch search results for whatever is searched
-                            foundSongs = await NetworkService.fetch(resultsFor: searchText)
-                        }
+                
+                TextField("Enter a song name", text: $searchText)
+
+                Button(action: {
+                    Task {
+                        //Fetch search results for whatever is searched
+                        foundSongs = await NetworkService.fetch(resultsFor: searchText)
                     }
+                }, label: {
+                    Text("Load results")
+                })
+                .buttonStyle(.borderedProminent)
                 
-                ForEach(foundSongs, id: \.trackId) { song in
-                    AudioPlayerView(urlOfAudioToPlay: song.previewUrl)
+                ScrollView {
+                    
+                    ForEach(foundSongs, id: \.trackId) { currentSong in
+                        
+                        VStack {
+                            
+                            Divider()
+                            
+                            HStack {
+                                
+                                RemoteImageView(urlOfImageToShow: currentSong.artworkUrl100)
+                                
+                                Text(currentSong.trackName)
+                                    .bold()
+                                
+                                Spacer()
+                            }
+                            Text(currentSong.collectionName)
+                                .italic()
+                            
+                            Text(currentSong.artistName)
+                            
+                            AudioPlayerView(urlOfAudioToPlay: currentSong.previewUrl)
+                                .padding(.top, 2)
+                                .padding(.horizontal, 5)
+                        }
+                        
+                        
+                    }
                 }
-                
             }
         }
     }
